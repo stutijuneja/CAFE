2f7007c1f97fd984d83f74b2bb0c27901ebb7831
Samarth <94822738+SamarthKumar3@users.noreply.github.com>
2024-01-21 03:49:04 +0530

    First commit
    css grid based menu, smooth scrolling effect, menu-navigator created- needs sticky position JS functionality

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+});
+
 window.addEventListener("load", function () {
   var pageLoader = document.querySelector(".page-loader");
   pageLoader.style.display = "none";
 });
+
+
