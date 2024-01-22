b242c7c193a1f9c39bd3570aedeb429f2f7feb58
Samarth <94822738+SamarthKumar3@users.noreply.github.com>
2024-01-23 03:52:49 +0530

    second commit
    responsiveness, styling, menu navigator fixed

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-});
-
 window.addEventListener("load", function () {
   var pageLoader = document.querySelector(".page-loader");
   pageLoader.style.display = "none";
 });
 
+window.addEventListener("scroll", () => {
+  let menu = document.querySelector('.menu-navigator');
+  let content = document.querySelector('.content').offsetTop;
 
+  let scrollPos = window.scrollY || document.documentElement.scrollTop;
+  if (content < scrollPos) {
+    menu.classList.add("sticky")
+  } else {
+    menu.classList.remove("sticky");
+  }
+})
